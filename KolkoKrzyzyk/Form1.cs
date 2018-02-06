b333e39fb31d25e123f9e8c20060f078ce6d3040
adiZ33 <adix2562@gmail.com>
2018-02-06 23:08:35 +0100

    Początek
    Układ przycisków, pierwsze funkcje

---
+++ KolkoKrzyzyk/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KolkoKrzyzyk
+{
+    public partial class Form1 : Form
+    {
+
+        bool turn = true; //1=X 0=O
+        int turnCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By adiZ", "Tic Tac Toe About");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+
+            turn = !turn;
+            b.Enabled = false;
+
+        }
+    }
+}
